a42eef0df7cebbc1b008d26985f6313d3aeb49c8
giuliomantovi <giulio.mantovi@libero.it>
2024-02-14 23:11:34 +0100

    separated whisper and faster whisper in 2 functions

--- main.py
+++ main.py
@@ -2,15 +2,21 @@
 from Config import Constants
 from spleeter.separator import Separator
 from faster_whisper import WhisperModel
+import os
+
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 import warnings
+
 warnings.filterwarnings('ignore')
+
 
 def source_separation():
     separator = Separator('spleeter:5stems')
     separator.separate_to_file(Constants.INPUT_AUDIO, Constants.OUTPUT_AUDIO)
 
-def transcript():
-    model_size = "large-v3"
+
+def fast_transcript():
+    model_size = "small"
     # Run on GPU with FP16
     model = WhisperModel(model_size, device="cuda", compute_type="float16")
 
@@ -18,22 +24,27 @@ def transcript():
     # model = WhisperModel(model_size, device="cuda", compute_type="int8_float16")
     # or run on CPU with INT8
     # model = WhisperModel(model_size, device="cpu", compute_type="int8")
-    segments, info = model.transcribe("audio.mp3", beam_size=5)
+    segments, info = model.transcribe(Constants.INPUT_AUDIO, beam_size=5)
 
     print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
 
     for segment in segments:
         print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
-
-    #model = whisper.load_model("base")
-    #result = model.transcribe(Constants.INPUT_AUDIO)
-    #with (open("Config/output_audio/transcription.txt", "w") as f):
+    # WHISPER AI WITHOUT CUDA
+    # model = whisper.load_model("base")
+    # result = model.transcribe(Constants.INPUT_AUDIO)
+    # with (open("Config/output_audio/transcription.txt", "w") as f):
     #    f.write(result["text"])
 
+
+def slow_transcript():
+    # WHISPER AI WITHOUT CUDA
+    model = whisper.load_model("base")
+    result = model.transcribe(Constants.INPUT_AUDIO)
+    with (open("Config/output_audio/transcription.txt", "w") as f):
+        f.write(result["text"])
+
+
 if __name__ == '__main__':
-    source_separation()
-    #transcript()
-
-
-
-
+    # source_separation()
+    slow_transcript()
