43225a2203b9597984ff0e8decde0a5b194d9086
giuliomantovi <giulio.mantovi@libero.it>
2024-02-15 13:46:44 +0100

    adjusted microphone_input.py

--- microphone_input.py
+++ microphone_input.py
@@ -7,49 +7,62 @@ CHUNK = 1024
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 44100
-RECORD_SECONDS = 5
-WAVE_OUTPUT_FILENAME = "output.wav"
+#RECORD_SECONDS = 5
+WAVE_OUTPUT_FILENAME = "Config/output_audio/mic_output.wav"
+device_index=1
 
-p = pyaudio.PyAudio()
+audio = pyaudio.PyAudio()
 
-stream = p.open(format=FORMAT,
-                channels=CHANNELS,
-                rate=RATE,
-                input=True,
-                frames_per_buffer=CHUNK)
+def setMicrophone():
+    print("----------------------record device list---------------------")
+    info = audio.get_host_api_info_by_index(0)
+    numdevices = info.get('deviceCount')
+    for i in range(0, numdevices):
+        if (audio.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+            print("Input Device id ", i, " - ", audio.get_device_info_by_host_api_device_index(0, i).get('name'))
+    print("-------------------------------------------------------------")
+    return int(input())
 
-frames = []
-print("Press space to start recording")
-keyboard.wait('space')
-print("Recording...press space to stop")
-time.sleep(0.2)
+def record():
+    stream = audio.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    input_device_index = device_index,
+                    frames_per_buffer=CHUNK)
 
-#version with keyboard input
-while True:
-    try:
+    frames = []
+    print("Press space to start recording")
+    keyboard.wait('space')
+    print("Recording...press space to stop")
+    time.sleep(0.2)
+
+    #version with keyboard input
+    while True:
+        try:
+            data = stream.read(CHUNK)
+            frames.append(data)
+        except KeyboardInterrupt:
+            break
+        if keyboard.is_pressed('space'):
+            print("Stopping record after brief delay")
+            time.sleep(0.2)
+            break
+
+    #version with preset seconds
+    """for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
         data = stream.read(CHUNK)
         frames.append(data)
-    except KeyboardInterrupt:
-        break
-    if keyboard.is_pressed('space'):
-        print("Stopping record after brief delay")
-        time.sleep(0.2)
-        break
+    
+    print("* done recording")"""
 
-#version with preset seconds
-"""for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-    data = stream.read(CHUNK)
-    frames.append(data)
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
 
-print("* done recording")"""
-
-stream.stop_stream()
-stream.close()
-p.terminate()
-
-wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-wf.setnchannels(CHANNELS)
-wf.setsampwidth(p.get_sample_size(FORMAT))
-wf.setframerate(RATE)
-wf.writeframes(b''.join(frames))
-wf.close()
+    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(audio.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close()
